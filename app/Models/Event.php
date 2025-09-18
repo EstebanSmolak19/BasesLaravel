845647b5520eacc163b469b4f9e4009050dec482
SMOLAK <esteban.smolak@saint-benigne.fr>
2025-09-18 09:19:17 +0200

    feat: Test Acesseur

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -22,5 +23,18 @@ class Event extends Model
 
     public function Type() {
         return $this->belongsTo(Type::class, 'TypeId');
+    }
+
+    /**
+     * Acesseur qui renvoie et ajoute en MAJ (la renommer comme le champ que l'on souhaite manipuler)
+     *
+     * @return Attribute
+     */
+    protected function TestAcessseur(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => $value,
+            get: fn ($value) => strtoupper($value)           
+        );
     }
 }
